66059b0ad1b180416e0401da5e18c6fe5249163a
lucasbr023 <lmc3@cin.ufpe.br>
2015-01-24 22:20:11 -0300

    mudanças

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+  class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
 
